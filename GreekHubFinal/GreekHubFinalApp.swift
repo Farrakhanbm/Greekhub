4c873289909302837c64b2ee12b8cb406d345da0
Farrakhan Muhammad <FarrakhanMuhammad1@gmail.com>
2026-04-14 21:06:21 -0400

    Initial Commit

---
+++ GreekHubFinal/GreekHubFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreekHubFinalApp.swift
+//  GreekHubFinal
+//
+//  Created by Farrakhan Muhammad on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct GreekHubFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
